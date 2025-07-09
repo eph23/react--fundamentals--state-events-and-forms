71bb0c6f041a1bcf5dfaa5c1418b98113baf0852
ephraim <ephraims.818@gmail.com>
2025-07-09 20:06:17 +0600

    REACT--FUNDAMENTALS--STATE-EVENTS-AND-FORMS - 09. Rendering the Items List

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 2, description: "Charger", quantity: 12, packed: true },
+];
+
 function App() {
     return (
         <div className="App">
@@ -24,8 +30,23 @@ function Form() {
 function PackingList() {
     return (
         <div className="list">
-            <h3>List</h3>
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
         </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
     );
 }
 
