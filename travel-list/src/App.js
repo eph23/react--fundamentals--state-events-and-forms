231a37218d722ea52f62be73af75cdfb422eb5f0
ephraim <ephraims.818@gmail.com>
2025-07-09 18:17:42 +0600

    REACT--FUNDAMENTALS--STATE-EVENTS-AND-FORMS - 08. Building the Layout

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,40 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Stats />
+        </div>
+    );
+}
+
+function Logo() {
+    return <h1>🌴 Far away 💼</h1>;
+}
+
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return (
+        <div className="list">
+            <h3>List</h3>
+        </div>
+    );
+}
+
+function Stats() {
+    return (
+        <footer className="stats">
+            <em>🧳 You have X items on your list and you already packed</em>
+        </footer>
+    );
 }
 
 export default App;
